2ac750061b46ed56701a74666f615805c2247ed7
Rheniel000 <johnrheniel.deguzman@gmail.com>
2025-06-04 18:26:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+
+  if (taskText === "") return;
+
+  const taskList = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+
+  li.textContent = taskText;
+
+  li.addEventListener("click", function () {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌";
+  deleteBtn.onclick = function () {
+    taskList.removeChild(li);
+  };
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  input.value = "";
+}
